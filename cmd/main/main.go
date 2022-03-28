aa442d0bb4e885b36fa778106f758f96a5d44aa9
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-28 10:33:10 +0530

    Fixed main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"github.com/TheFenrisLycaon/LibraryMS/pkg/routes"
-	"github.com/gorilla/mux"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/TheFenrisLycaon/LibraryMS/pkg/routes"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 	routes.RegisterLibraryRoutes(r)
 	http.Handle("/", r)
+	fmt.Println("Server started at port 9010")
 	log.Fatal(http.ListenAndServe(":9010", r))
 }
